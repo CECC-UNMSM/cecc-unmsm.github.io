48491d3024cf55645304714a1ebed61b337fa713
Diego <ddsgonas@gmail.com>
2025-06-11 12:49:46 -0500

    cambio

--- src/js/ageFusm.js
+++ src/js/ageFusm.js
@@ -5,7 +5,11 @@ const HTMLResponse = document.querySelector("#container-app");
 fetch(`${API_URL}/sintesiss.json`)
     .then((response) => response.json())
     .then((sintesiss) => {
-        sintesiss.forEach((sintesis) => {
+        // Convertir objeto a array si es necesario
+        let arraySintesiss = Array.isArray(sintesiss) ? sintesiss : Object.values(sintesiss);
+        
+        // Revertir el orden
+        arraySintesiss.reverse().forEach((sintesis) => {
             const article = document.createElement('article');
             
             let h1F = document.createElement("h1");           
@@ -17,12 +21,10 @@ fetch(`${API_URL}/sintesiss.json`)
             a.innerText = "Click para ver";
             a.href = sintesis.contenido;
 
-            article.appendChild(h1F)
-            article.appendChild(h1)
-            article.appendChild(a)
+            article.appendChild(h1F);
+            article.appendChild(h1);
+            article.appendChild(a);
 
-            HTMLResponse.appendChild(article)
+            HTMLResponse.appendChild(article);
         });
     });
-
-
